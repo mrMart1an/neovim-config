70ba9bc0c429a6bc370b036db87f8c6edf80cac3
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2025-07-24 16:10:28 +0200

    new keybind

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -28,3 +28,22 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTre
 
 -- toggle line wrap
 vim.keymap.set('n', '<leader>lw', ':set wrap!<CR>', { desc = 'Toggle line wraping'})
+
+-- Format the buffer using the LSP
+vim.keymap.set('n', '<leader>lfb', vim.lsp.buf.format, { desc = 'Format the buffer with the LSP formatter' })
+
+-- Fix extremely annoying issue with tab 
+function leave_snippet()
+    if
+        ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+        require('luasnip').unlink_current()
+    end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua leave_snippet()
+]])
